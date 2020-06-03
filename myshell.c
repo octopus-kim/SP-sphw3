8c44fc25e79c23e0552c92207d49cc375754b215
octopus-kim <kk97sj@gmail.com>
2020-06-03 20:07:13 +0900

    f1. main()   1. check argument

--- myshell.c
+++ myshell.c
@@ -5,18 +5,40 @@
 
 int main(int argc, char **argv)
 {
-    // myshell -c [arg] [arg] ...
-    if (argc > 1) {
-        // check argument
-        if (strcmp(argv[1], "-c") != 0) {
-            printf("Usage : %s -c [arg] [arg] ...\n", argv[0]); return -1;
-        }
-        printf("%d arg\n", argc - 1);
-    }
+    int cmd_len, count;
+    int temp, i, j;
+    char **cmd;
+    if (argc == 1) {
 
-    // myshell
-    else {
-        printf("0 arg\n");
+    }
+    else if (argc == 3) {
+        if (strcmp(argv[1], "-c") != 0) {
+            printf("Usage : %s [-c] [commend]\n", argv[0]); return -1;
+        }
+
+        cmd_len = strlen(argv[2]);
+        count = 1;
+        for (i = 0; i < cmd_len; i++) {
+            if (strcmp(argv[2][i], " ") == 0) count++;
+        }
+
+        cmd = (char**)malloc(sizeof(char*) * count);
+        for (i = 0; i < count; i++)
+            cmd[i] = (char*)malloc(sizeof(char) * 10);
+
+        temp = 0; j = 0;
+        for (i = 0; i < cmd_len; i++) {
+            if (strcmp(argv[2][i], " ") != 0) {
+                j++; temp = 0; continue;
+            }
+            cmd[j][temp] = argv[2][i];
+            temp++;
+        }
+
+        for (i = 0; i < count; i++)
+            printf("%s\n", cmd[i]);
+    } else {
+        printf("Usage : %s [-c] [commend]\n", argv[0]); return -1;
     }
 
     return 0;
