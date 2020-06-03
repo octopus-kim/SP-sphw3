3e2bf5b924f0dce2d0d3b28cc2d64a9afb40882b
octopus-kim <kk97sj@gmail.com>
2020-06-03 18:10:15 +0900

    [test] 1. execlp 2. cmd[] 3. header file

--- myshell.c
+++ myshell.c
@@ -4,15 +4,15 @@
 int main(int argc, char **argv)
 {
     while(1) {
-        printPrompt();
-        char *cmd = get_next_command();
+        printf("$ ");
+        char *cmd = &argv;
 
         int child_pid = fork();
         if (child_pid == 0) {
-            print("%s, %s, %s\n", cmd[0], cmd[1], cmd[2])
+            print("%s, %s, %s\n", cmd[0], cmd[1], cmd[2]);
             execlp(cmd[0], cmd[1], cmd[2]);
 
-            panic("exec failed\n")
+            printf("exec failed\n");
         } else {
             waitpid(child_pid);
         }
