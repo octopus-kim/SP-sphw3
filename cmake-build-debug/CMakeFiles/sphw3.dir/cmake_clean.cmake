e2b5082f9ae1897728008324186e6c02b601c611
octopus-kim <kk97sj@gmail.com>
2020-06-03 14:58:34 +0900

    create master branch

---
+++ cmake-build-debug/CMakeFiles/sphw3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sphw3.dir/main.c.obj"
+  "sphw3.exe"
+  "sphw3.exe.manifest"
+  "sphw3.lib"
+  "sphw3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sphw3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/sphw3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sphw3.
+# This may be replaced when dependencies are built.
