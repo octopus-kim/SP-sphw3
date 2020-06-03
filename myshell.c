5c25a0dafc2ac1902305afb12d177e78f20a5d8b
octopus-kim <kk97sj@gmail.com>
2020-06-03 18:56:36 +0900

    f1. main()   1. check argument

--- myshell.c
+++ myshell.c
@@ -4,11 +4,15 @@
 
 int main(int argc, char **argv)
 {
+    int i = 0;
     // myshell -c [arg] [arg] ...
     if (argc > 1) {
         // check argument
         if (argv[1] != "-c") {
-            printf("Usage : %s -c [arg] [arg] ...\n", argv[0]); return -1;
+            printf("Usage : %s -c [arg] [arg] ...\n", argv[0]);
+            for (i = 0; i < argc; i++)
+                printf("%s\n", argv[i]);
+            return -1;
         }
         printf("%d arg\n", argc - 1);
     }
